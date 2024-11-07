8422643f02d73d3ca8fcc4a0030db8ed1588c1d9
Miguel A. Fuentes Buchholtz <1669317+miguelantonio@users.noreply.github.com>
2024-11-06 23:50:22 -0300

    Update verify-no-criminal-history.py
    Signed-off-by: Miguel A. Fuentes Buchholtz <1669317+miguelantonio@users.noreply.github.com>

--- nape-tutorials/tutorial-1/simple-business-process/new-employee/activity/criminal-history/verify-no-criminal-history.py
+++ nape-tutorials/tutorial-1/simple-business-process/new-employee/activity/criminal-history/verify-no-criminal-history.py
@@ -4,24 +4,36 @@ def evaluate(evidence):
         document_text = "".join(evidence)
 
         # Find the start of the "Criminal History" section
-        criminal_history_start = document_text.find("Criminal History")
+        criminal_history_start = document_text.find("REGISTRO GENERAL DE CONDENAS")
         
         # Find the start of the next section after "Criminal History"
         next_section_start = document_text.find("\n\n", criminal_history_start)
 
+        # Find the start of the "Criminal History" section
+        violence_criminal_history_start = document_text.find("VIOLENCIA INTRAFAMILIAR")
+        
+        # Find the start of the next section after "Special violence in family Criminal History"
+        violence_next_section_start = document_text.find("\n\n", criminal_history_start)
+
         # Extract the "Criminal History" section
-        if criminal_history_start == -1:
-            return "inconclusive", "The 'Criminal History' section cannot be found."
-        elif next_section_start == -1:
+        if criminal_history_start == -1 || violence_criminal_history_start == -1:
+            return "inconclusive", "Either the 'REGISTRO GENERAL DE CONDENAS' section or the special 'VIOLENCIA INTRAFAMILIAR' section cannot be found."
+        
+        if next_section_start == -1:
             criminal_history_section = document_text[criminal_history_start:]
         else:
             criminal_history_section = document_text[criminal_history_start:next_section_start]
 
-        # Determine the outcome based on the content of the "Criminal History" section
-        if "No criminal records found" in criminal_history_section:
-            return "pass", "The evidence contains the expected statement: 'No criminal records found'."
+        if next_section_start == -1:
+            violence_criminal_history_section = document_text[violence_criminal_history_start:]
+        else:
+            violence_criminal_history_section = document_text[violence_criminal_history_start:violence_next_section_start]
+
+        # Determine the outcome based on the content of the "REGISTRO GENERAL DE CONDENAS" and "VIOLENCIA INTRAFAMILIAR" section
+        if "SIN ANTECEDENTESPARTICULARES" in criminal_history_section && "SIN ANOTACIONESPARTICULARES" in violence_criminal_history_section:
+            return "pass", "The evidence contains the expected statements: 'SIN ANTECEDENTESPARTICULARES' and 'SIN ANOTACIONESPARTICULARES'."
         elif "No criminal records found" not in criminal_history_section:
-            return "fail", "The evidence does not contain the expected statement: 'No criminal records found'."
+            return "fail", "The evidence does not contain either of these expected statements: 'SIN ANTECEDENTESPARTICULARES' or 'SIN ANOTACIONESPARTICULARES'."
 
     except Exception as e:
         # Return an error outcome with the error message
